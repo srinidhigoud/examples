6aff5cad9475f820ac8aac7771e50ae8cab72360
srinidhigoud <sgm400@nyu.edu>
2019-04-15 11:04:36 -0400

    proj

--- imagenet/run_all_models.sh
+++ imagenet/run_all_models.sh
@@ -4,4 +4,4 @@ sbatch alexnet_experiment.sh p40
 sbatch vgg16_experiment.sh p40
 sbatch resnet18_experiment.sh p40 
 sbatch densenet_experiment.sh p40
-sbatch inception_experiment.sh p40
+#sbatch inception_experiment.sh p40
