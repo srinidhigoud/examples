b6bf0b69fd5f4ddc9e4a15abc29465882fb3376b
srinidhigoud <srinidhi.goud29@gmail.com>
2019-04-16 12:49:02 -0400

    proj

--- imagenet/run_all_models.sh
+++ imagenet/run_all_models.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sbatch alexnet_experiment.sh p40
-sbatch vgg16_experiment.sh p40
-sbatch resnet18_experiment.sh p40 
-sbatch densenet_experiment.sh p40
-#sbatch inception_experiment.sh p40
+sbatch alexnet_experiment.sh
+sbatch vgg16_experiment.sh
+sbatch resnet18_experiment.sh
