7044d720e3939818c771da2fba529a112ac738c7
srinidhigoud <srinidhi.goud29@gmail.com>
2019-04-17 18:05:23 -0400

    proj

--- imagenet/subsectionExperiment_vgg16.sh
+++ imagenet/subsectionExperiment_vgg16.sh
@@ -3,7 +3,7 @@
 #SBATCH --verbose
 #SBATCH --job-name=subExpCloudML
 #SBATCH --mem=100GB
-#SBATCH --output=out.vgg16_64_0.01_k80_loss_only.%j
+#SBATCH --output=out.vgg16_64_0.01_p40_loss_only.%j
 
 ##SBATCH --time=100:00:00
 #SBATCH--gres=gpu:1
@@ -13,8 +13,8 @@
 #SBATCH --cpus-per-task=28
 #SBATCH --exclusive
 #SBATCH --time=00:60:00
-##SBATCH --gres=gpu:k80:4
-#SBATCH --gres=gpu:k80:4
+##SBATCH --gres=gpu:p40:4
+#SBATCH --gres=gpu:p40:4
 #SBATCH --reservation=chung
 
 # module load pytorch/python2.7/0.3.0_4
@@ -24,14 +24,14 @@
 module load python3/intel/3.6.3 cuda/9.0.176 nccl/cuda9.0/2.4.2
 
 source ~/pytorch_env/py3.6.3/bin/activate
-echo "vgg16_64_0.01_k80_loss_only"
+echo "vgg16_64_0.01_p40_loss_only"
 nvprof --timeout 300 --csv --profile-from-start off --print-summary python subsectionMain.py --arch vgg16 -b 64 --epochs 1 --lr 0.01 /beegfs/work/courses/2019-CSCI-GA-3033-025/imagenet_pytorch_small
 # nvprof --profile-from-start off --timeout 100 --metric gld_efficiency python subsectionMain.py -a alexnet -b 16 --epochs 1 --lr 0.01 /beegfs/work/courses/2019-CSCI-GA-3033-025/imagenet_pytorch_small
 # nvprof --timeout 1800 --csv --profile-from-start off --track-memory-allocations on --cpu-profiling on --print-summary python subsectionMain.py --arch alexnet -b 16 --epochs 1 --lr 0.001 /beegfs/work/courses/2019-CSCI-GA-3033-025/imagenet_pytorch_small
-# nvprof --timeout 1800 --output-profile alexnet_loss_only_16_0.001_small_k80_%p.nvvp --csv --log-file alexnet_loss_only_16_0.001_small_k80_%p.log --profile-from-start off --track-memory-allocations on --cpu-profiling on --print-summary python subsectionMain.py --arch alexnet -b 16 --epochs 1 --lr 0.001 /beegfs/work/courses/2019-CSCI-GA-3033-025/imagenet_pytorch_small
+# nvprof --timeout 1800 --output-profile alexnet_loss_only_16_0.001_small_p40_%p.nvvp --csv --log-file alexnet_loss_only_16_0.001_small_p40_%p.log --profile-from-start off --track-memory-allocations on --cpu-profiling on --print-summary python subsectionMain.py --arch alexnet -b 16 --epochs 1 --lr 0.001 /beegfs/work/courses/2019-CSCI-GA-3033-025/imagenet_pytorch_small
 
 
 # source ~/pytorch_env/py3.6.3/bin/activate
-# echo "alexnet_16_0.001_k80_loss_only"
-# nvprof --timeout 1800 --output-profile alexnet_loss_only_16_0.001_small_k80_%p.nvvp --csv --log-file alexnet_loss_only_16_0.001_small_k80_%p.log --profile-from-start off --track-memory-allocations on --cpu-profiling on --print-summary python subsectionMain.py --arch alexnet -b 16 --epochs 1 --lr 0.001 /beegfs/work/courses/2019-CSCI-GA-3033-025/imagenet_pytorch_small
-# # nvprof --output-profile alexnet_loss_only_16_0.001_small_k80_%p.nvvp --csv --log-file alexnet_loss_only_16_0.001_small_k80_%p.log --profile-from-start off --track-memory-allocations on --cpu-profiling on --print-summary python subsectionMain.py --arch alexnet -b 16 --epochs 1 --lr 0.001 /beegfs/work/courses/2019-CSCI-GA-3033-025/imagenet_pytorch_small
+# echo "alexnet_16_0.001_p40_loss_only"
+# nvprof --timeout 1800 --output-profile alexnet_loss_only_16_0.001_small_p40_%p.nvvp --csv --log-file alexnet_loss_only_16_0.001_small_p40_%p.log --profile-from-start off --track-memory-allocations on --cpu-profiling on --print-summary python subsectionMain.py --arch alexnet -b 16 --epochs 1 --lr 0.001 /beegfs/work/courses/2019-CSCI-GA-3033-025/imagenet_pytorch_small
+# # nvprof --output-profile alexnet_loss_only_16_0.001_small_p40_%p.nvvp --csv --log-file alexnet_loss_only_16_0.001_small_p40_%p.log --profile-from-start off --track-memory-allocations on --cpu-profiling on --print-summary python subsectionMain.py --arch alexnet -b 16 --epochs 1 --lr 0.001 /beegfs/work/courses/2019-CSCI-GA-3033-025/imagenet_pytorch_small
